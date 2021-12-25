9ddaabcb181f4f9662bf992a65d9c4aa23b382df
Puru Tuladhar <tuladharpuru@gmail.com>
2021-12-25 16:15:26 +0545

    Improve statistics format

--- internal/stats.go
+++ internal/stats.go
@@ -8,7 +8,7 @@ import (
 func (s *SSLHandshake) ShowStatistics() {
 	fmt.Println()
 	fmt.Printf("--- %s handshake statistics ---\n", s.Config.Endpoint)
-	fmt.Printf("%d handshakes sent, finished: %d, failed: %d, time: %d ms\n", s.State.Total, s.State.Finished, s.State.Failed, s.State.Elapsed)
-	fmt.Printf("rtt max: %d ms, avg: %d ms, min: %d ms\n", s.State.Max, s.State.Avg, s.State.Min)
+	fmt.Printf("%d handshakes sent, finish: %d, fail: %d, time: %dms\n", s.State.Total, s.State.Finished, s.State.Failed, s.State.Elapsed)
+	fmt.Printf("rtt min/avg/max: %d/%d/%d ms\n", s.State.Min, s.State.Avg, s.State.Max)
 	os.Exit(0)
 }
